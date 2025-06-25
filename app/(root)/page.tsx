cd3036e68ffd8a7f895f23ee066d1fa88595e3f3
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-06-25 22:41:34 +0300

    adding the deal countdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -27,6 +29,8 @@ const Homepage = async () => {
         limit={4}
       />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
